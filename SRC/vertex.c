44915cade73c851152bc253370b3acdbdc147f86
Friedrich Hemer <friedrich.hemer@gmail.com>
2015-01-07 17:42:40 +0100

    rm img_based creation of vertex

--- SRC/vertex.c
+++ SRC/vertex.c
@@ -42,61 +42,6 @@ Vertex* create_vertex( double x, double y, double z )
 	return new_vert;
 }
 
-void init_vertex_img_based( Vertex *vert, const PGMData *pic )
-{
-	double x = randf();
-	double y = randf();
-	double z;
-
-	int sizex = pic->row;
-	int sizey = pic->col;
-	int xpic = floor(x*sizex);
-	int ypic = floor(y*sizey);
-	double difx = (x*sizex)-xpic-0.5;
-	double dify = (y*sizey)-ypic-0.5;
-	int sensx = sgn(difx);
-	int sensy = sgn(dify);
-
-	// printf("sizex = %d, sizey = %d\n", sizex, sizey);
-	// printf("difx = %f, dify = %f\n", difx, dify);
-	// printf("xpic = %d, ypic = %d\n", xpic, ypic);
-
-	if (xpic <= 0 || xpic >= pic->row - 1 || ypic <=0 || ypic >= pic->col - 1)
-	{
-		z = (double) pic->matrix[xpic][ypic]/255;
-	}
-	else
-	{
-		int xy = pic->matrix[xpic][ypic];
-		int xul = pic->matrix[xpic+sensx][ypic];
-		int yul = pic->matrix[xpic][ypic+sensy];
-		z = ( (difx*sensx*xul + (1-difx*sensx)*xy) + (dify*sensy*yul + (1-dify*sensy)*xy) ) / (2*255);
-	}
-
-	vert->coords[0] = x;
-	vert->coords[1] = y;	
-	vert->coords[2] = z/2;
-
-	for(int i=0; i<NB_LNK; i++)
-	{
-		vert->links[i][BWD] = NULL;	
-		vert->links[i][FWD] = NULL;
-	}
-
-	vert->zdist = 0;
-}
-
-Vertex* create_vertex_img_based( const PGMData *pic )
-{
-	Vertex *new_vert = (Vertex *) malloc(sizeof(Vertex));
-
-	if (new_vert != NULL)
-		init_vertex_img_based( new_vert, pic);
-	else
-		printf("Error in create_vertex_img_based function\n");
-
-	return new_vert;
-}
 
 int is_superior_vertex( Vertex *p1, Vertex *p2 )
 {
--- SRC/vertex.h
+++ SRC/vertex.h
@@ -47,24 +47,6 @@ void init_vertex( Vertex *vert, double x, double y, double z ) ;
 Vertex* create_vertex( double x, double y, double z ) ;
 
 /**
- * @brief Initialize a vertex based on an image
- * @details Read the grayscale value of the image
- * 
- * @param vert The vertex to initialize
- * @param pic The input image
- */
-void init_vertex_img_based( Vertex *vert, const PGMData *pic ) ;
-
-/**
- * @brief Create a new vertex based on an image
- * @details Do a malloc and call init_vertex_img_based()
- * 
- * @param pic The input image
- * @return The new vertex created
- */
-Vertex* create_vertex_img_based( const PGMData *pic ) ;
-
-/**
  * @brief Compare vertices by there zdist
  * @details is p1 > p2 ?
  * 
